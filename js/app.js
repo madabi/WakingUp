e38f39337663d174d5eec8229b6c5b554e510240
Lea Boesch <lea.boesch@students.fhnw.ch>
2016-06-13 18:36:24 +0200

    sha1 in eigenes file ausgelagert

--- js/app.js
+++ js/app.js
@@ -64,7 +64,7 @@ jQuery(document).ready(function () {
     //profile
     $('#loginButton').on('click', function (event) {
         event.preventDefault();
-        if(tryLoginAuth()) {
+        if (tryLoginAuth()) {
             showLoggedInAccountView();
         }
         emptyForm($('#login'));
@@ -154,7 +154,6 @@ jQuery(document).ready(function () {
     }
 
 
-
     /*
      *  Inserat löschen
      *
@@ -178,7 +177,6 @@ jQuery(document).ready(function () {
             });
         }
     }
-
 
 
     /*
@@ -230,8 +228,8 @@ jQuery(document).ready(function () {
             $('#myAdsList').append($('<li><span id="noContentIcon" class="glyphicon glyphicon-list-alt" aria-hidden="true"></span></li>' +
                 '<li id="noContentText">Du hast noch keine Inserate erstellt.</li>'));
 
-           /* adTable = adTable.concat('<tr><td><span id="noContentIcon" class="glyphicon glyphicon-list-alt" aria-hidden="true"></span></td></tr>+' +
-                '<tr><td id="noContentText">Du hast noch keine Inserate erstellt.</td></tr>');*/
+            /* adTable = adTable.concat('<tr><td><span id="noContentIcon" class="glyphicon glyphicon-list-alt" aria-hidden="true"></span></td></tr>+' +
+             '<tr><td id="noContentText">Du hast noch keine Inserate erstellt.</td></tr>');*/
         } else {
 
             for (var i = 0; i < data.length; i++) {
@@ -242,7 +240,7 @@ jQuery(document).ready(function () {
                 var year = date.substr(0, 4);
 
                 var adTable = $('<table></table>');
-                var newListElement = $('<li id="' +data[i].id + '"></li>');
+                var newListElement = $('<li id="' + data[i].id + '"></li>');
                 var formattedDate = day + ' - ' + month + ' - ' + year;
 
                 adTable = adTable.append($('<tr><td><h5>' + data[i].title + '</h5></td><td><span class="glyphicon glyphicon-trash"></span></td></tr>' +
@@ -293,7 +291,6 @@ jQuery(document).ready(function () {
         });
         return loginSuccessful;
     }
-
 
 
     /*
@@ -352,7 +349,6 @@ jQuery(document).ready(function () {
     }
 
 
-
     /*
      *  Loggt den Benutzer direkt ein.
      *
@@ -382,11 +378,10 @@ jQuery(document).ready(function () {
     }
 
 
-
     /*
-    * Überprüft, ob im Browser-Local Storage ein gültiges Token vorhanden ist.
-    *
-    * @return boolean
+     * Überprüft, ob im Browser-Local Storage ein gültiges Token vorhanden ist.
+     *
+     * @return boolean
      */
     function verifyToken() {
 
@@ -414,10 +409,10 @@ jQuery(document).ready(function () {
 
 
     /*
-    *  Schreibt ein Token in den Local Storage des Browsers
-    *
-    *  @param token: Zu setzendes Token
-    */
+     *  Schreibt ein Token in den Local Storage des Browsers
+     *
+     *  @param token: Zu setzendes Token
+     */
     function setToken(token) {
 
         localStorage.setItem('wakingUp_token', token);
@@ -467,308 +462,6 @@ jQuery(document).ready(function () {
         } else {
             return false;
         }
-    }
-
-
-    /*
-     *  Hashfunktion zur Verschleierung des Passworts
-     *
-     *  @param msg: Zu hashender String
-     *  @return: gehashter String
-     *
-     *  Quelle: http://www.webtoolkit.info/
-     */
-    function sha1(msg) {
-
-
-        var blockstart;
-
-        var i, j;
-
-        var W = new Array(80);
-
-        var H0 = 0x67452301;
-
-        var H1 = 0xEFCDAB89;
-
-        var H2 = 0x98BADCFE;
-
-        var H3 = 0x10325476;
-
-        var H4 = 0xC3D2E1F0;
-
-        var A, B, C, D, E;
-
-        var temp;
-
-
-        msg = Utf8Encode(msg);
-
-
-        var msg_len = msg.length;
-
-
-        var word_array = new Array();
-
-        for (i = 0; i < msg_len - 3; i += 4) {
-
-            j = msg.charCodeAt(i) << 24 | msg.charCodeAt(i + 1) << 16 |
-
-                msg.charCodeAt(i + 2) << 8 | msg.charCodeAt(i + 3);
-
-            word_array.push(j);
-
-        }
-
-
-        switch (msg_len % 4) {
-
-            case 0:
-
-                i = 0x080000000;
-
-                break;
-
-            case 1:
-
-                i = msg.charCodeAt(msg_len - 1) << 24 | 0x0800000;
-
-                break;
-
-
-            case 2:
-
-                i = msg.charCodeAt(msg_len - 2) << 24 | msg.charCodeAt(msg_len - 1) << 16 | 0x08000;
-
-                break;
-
-
-            case 3:
-
-                i = msg.charCodeAt(msg_len - 3) << 24 | msg.charCodeAt(msg_len - 2) << 16 | msg.charCodeAt(msg_len - 1) << 8 | 0x80;
-
-                break;
-
-        }
-
-
-        word_array.push(i);
-
-
-        while ((word_array.length % 16) != 14) word_array.push(0);
-
-
-        word_array.push(msg_len >>> 29);
-
-        word_array.push((msg_len << 3) & 0x0ffffffff);
-
-
-        for (blockstart = 0; blockstart < word_array.length; blockstart += 16) {
-
-
-            for (i = 0; i < 16; i++) W[i] = word_array[blockstart + i];
-
-            for (i = 16; i <= 79; i++) W[i] = rotate_left(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
-
-
-            A = H0;
-
-            B = H1;
-
-            C = H2;
-
-            D = H3;
-
-            E = H4;
-
-
-            for (i = 0; i <= 19; i++) {
-
-                temp = (rotate_left(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
-
-                E = D;
-
-                D = C;
-
-                C = rotate_left(B, 30);
-
-                B = A;
-
-                A = temp;
-
-            }
-
-
-            for (i = 20; i <= 39; i++) {
-
-                temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
-
-                E = D;
-
-                D = C;
-
-                C = rotate_left(B, 30);
-
-                B = A;
-
-                A = temp;
-
-            }
-
-
-            for (i = 40; i <= 59; i++) {
-
-                temp = (rotate_left(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
-
-                E = D;
-
-                D = C;
-
-                C = rotate_left(B, 30);
-
-                B = A;
-
-                A = temp;
-
-            }
-
-
-            for (i = 60; i <= 79; i++) {
-
-                temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
-
-                E = D;
-
-                D = C;
-
-                C = rotate_left(B, 30);
-
-                B = A;
-
-                A = temp;
-
-            }
-
-
-            H0 = (H0 + A) & 0x0ffffffff;
-
-            H1 = (H1 + B) & 0x0ffffffff;
-
-            H2 = (H2 + C) & 0x0ffffffff;
-
-            H3 = (H3 + D) & 0x0ffffffff;
-
-            H4 = (H4 + E) & 0x0ffffffff;
-
-
-        }
-
-
-        temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
-
-        return temp.toLowerCase();
-
-    }
-
-
-    function rotate_left(n, s) {
-
-        var t4 = ( n << s ) | (n >>> (32 - s));
-
-        return t4;
-
-    }
-
-    function lsb_hex(val) {
-
-        var str = "";
-
-        var i;
-
-        var vh;
-
-        var vl;
-
-
-        for (i = 0; i <= 6; i += 2) {
-
-            vh = (val >>> (i * 4 + 4)) & 0x0f;
-
-            vl = (val >>> (i * 4)) & 0x0f;
-
-            str += vh.toString(16) + vl.toString(16);
-
-        }
-
-        return str;
-
-    }
-
-
-    function cvt_hex(val) {
-
-        var str = "";
-
-        var i;
-
-        var v;
-
-
-        for (i = 7; i >= 0; i--) {
-
-            v = (val >>> (i * 4)) & 0x0f;
-
-            str += v.toString(16);
-
-        }
-
-        return str;
-
-    }
-
-    function Utf8Encode(string) {
-
-        string = string.replace(/\r\n/g, "\n");
-
-        var utftext = "";
-
-
-        for (var n = 0; n < string.length; n++) {
-
-
-            var c = string.charCodeAt(n);
-
-
-            if (c < 128) {
-
-                utftext += String.fromCharCode(c);
-
-            }
-
-            else if ((c > 127) && (c < 2048)) {
-
-                utftext += String.fromCharCode((c >> 6) | 192);
-
-                utftext += String.fromCharCode((c & 63) | 128);
-
-            }
-
-            else {
-
-                utftext += String.fromCharCode((c >> 12) | 224);
-
-                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-
-                utftext += String.fromCharCode((c & 63) | 128);
-
-            }
-
-
-        }
-
-
-        return utftext;
-
     }
 
 });
--- js/sha1.js
+++ js/sha1.js
@@ -1,162 +1,148 @@
-/**
- * Created by leaboesch on 06.06.16.
- */
 
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-/*  SHA-1 implementation in JavaScript                  (c) Chris Veness 2002-2014 / MIT Licence  */
-/*                                                                                                */
-/*  - see http://csrc.nist.gov/groups/ST/toolkit/secure_hashing.html                              */
-/*        http://csrc.nist.gov/groups/ST/toolkit/examples.html                                    */
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-
-/* jshint node:true *//* global define, escape, unescape */
-'use strict';
-
-
-/**
- * SHA-1 hash function reference implementation.
+/*
+ *  Hashfunktion zur Verschleierung des Passworts
  *
- * @namespace
- */
-var Sha1 = {};
-
-
-/**
- * Generates SHA-1 hash of string.
+ *  @param msg: Zu hashender String
+ *  @return: gehashter String
  *
- * @param   {string} msg - (Unicode) string to be hashed.
- * @returns {string} Hash of msg as hex character string.
+ *  Quelle: http://www.webtoolkit.info/
  */
-Sha1.hash = function(msg) {
-    // convert string to UTF-8, as SHA only deals with byte-streams
-    msg = msg.utf8Encode();
-
-    // constants [§4.2.1]
-    var K = [ 0x5a827999, 0x6ed9eba1, 0x8f1bbcdc, 0xca62c1d6 ];
-
-    // PREPROCESSING
-
-    msg += String.fromCharCode(0x80);  // add trailing '1' bit (+ 0's padding) to string [§5.1.1]
-
-    // convert string msg into 512-bit/16-integer blocks arrays of ints [§5.2.1]
-    var l = msg.length/4 + 2; // length (in 32-bit integers) of msg + ‘1’ + appended length
-    var N = Math.ceil(l/16);  // number of 16-integer-blocks required to hold 'l' ints
-    var M = new Array(N);
-
-    for (var i=0; i<N; i++) {
-        M[i] = new Array(16);
-        for (var j=0; j<16; j++) {  // encode 4 chars per integer, big-endian encoding
-            M[i][j] = (msg.charCodeAt(i*64+j*4)<<24) | (msg.charCodeAt(i*64+j*4+1)<<16) |
-                (msg.charCodeAt(i*64+j*4+2)<<8) | (msg.charCodeAt(i*64+j*4+3));
-        } // note running off the end of msg is ok 'cos bitwise ops on NaN return 0
-    }
-    // add length (in bits) into final pair of 32-bit integers (big-endian) [§5.1.1]
-    // note: most significant word would be (len-1)*8 >>> 32, but since JS converts
-    // bitwise-op args to 32 bits, we need to simulate this by arithmetic operators
-    M[N-1][14] = ((msg.length-1)*8) / Math.pow(2, 32); M[N-1][14] = Math.floor(M[N-1][14]);
-    M[N-1][15] = ((msg.length-1)*8) & 0xffffffff;
-
-    // set initial hash value [§5.3.1]
+function sha1(msg)
+{
+    var blockstart;
+    var i, j;
+    var W = new Array(80);
     var H0 = 0x67452301;
-    var H1 = 0xefcdab89;
-    var H2 = 0x98badcfe;
+    var H1 = 0xEFCDAB89;
+    var H2 = 0x98BADCFE;
     var H3 = 0x10325476;
-    var H4 = 0xc3d2e1f0;
-
-    // HASH COMPUTATION [§6.1.2]
-
-    var W = new Array(80); var a, b, c, d, e;
-    for (var i=0; i<N; i++) {
-
-        // 1 - prepare message schedule 'W'
-        for (var t=0;  t<16; t++) W[t] = M[i][t];
-        for (var t=16; t<80; t++) W[t] = Sha1.ROTL(W[t-3] ^ W[t-8] ^ W[t-14] ^ W[t-16], 1);
-
-        // 2 - initialise five working variables a, b, c, d, e with previous hash value
-        a = H0; b = H1; c = H2; d = H3; e = H4;
-
-        // 3 - main loop
-        for (var t=0; t<80; t++) {
-            var s = Math.floor(t/20); // seq for blocks of 'f' functions and 'K' constants
-            var T = (Sha1.ROTL(a,5) + Sha1.f(s,b,c,d) + e + K[s] + W[t]) & 0xffffffff;
-            e = d;
-            d = c;
-            c = Sha1.ROTL(b, 30);
-            b = a;
-            a = T;
-        }
-
-        // 4 - compute the new intermediate hash value (note 'addition modulo 2^32')
-        H0 = (H0+a) & 0xffffffff;
-        H1 = (H1+b) & 0xffffffff;
-        H2 = (H2+c) & 0xffffffff;
-        H3 = (H3+d) & 0xffffffff;
-        H4 = (H4+e) & 0xffffffff;
+    var H4 = 0xC3D2E1F0;
+    var A, B, C, D, E;
+    var temp;
+    msg = Utf8Encode(msg);
+    var msg_len = msg.length;
+    var word_array = new Array();
+    for (i = 0; i < msg_len - 3; i += 4) {
+        j = msg.charCodeAt(i) << 24 | msg.charCodeAt(i + 1) << 16 |
+            msg.charCodeAt(i + 2) << 8 | msg.charCodeAt(i + 3);
+        word_array.push(j);
     }
-
-    return Sha1.toHexStr(H0) + Sha1.toHexStr(H1) + Sha1.toHexStr(H2) +
-        Sha1.toHexStr(H3) + Sha1.toHexStr(H4);
-};
-
-
-/**
- * Function 'f' [§4.1.1].
- * @private
- */
-Sha1.f = function(s, x, y, z)  {
-    switch (s) {
-        case 0: return (x & y) ^ (~x & z);           // Ch()
-        case 1: return  x ^ y  ^  z;                 // Parity()
-        case 2: return (x & y) ^ (x & z) ^ (y & z);  // Maj()
-        case 3: return  x ^ y  ^  z;                 // Parity()
+    switch (msg_len % 4) {
+        case 0:
+            i = 0x080000000;
+            break;
+        case 1:
+            i = msg.charCodeAt(msg_len - 1) << 24 | 0x0800000;
+            break;
+        case 2:
+            i = msg.charCodeAt(msg_len - 2) << 24 | msg.charCodeAt(msg_len - 1) << 16 | 0x08000;
+            break;
+        case 3:
+            i = msg.charCodeAt(msg_len - 3) << 24 | msg.charCodeAt(msg_len - 2) << 16 | msg.charCodeAt(msg_len - 1) << 8 | 0x80;
+            break;
     }
-};
-
-/**
- * Rotates left (circular left shift) value x by n positions [§3.2.5].
- * @private
- */
-Sha1.ROTL = function(x, n) {
-    return (x<<n) | (x>>>(32-n));
-};
-
-
-/**
- * Hexadecimal representation of a number.
- * @private
- */
-Sha1.toHexStr = function(n) {
-    // note can't use toString(16) as it is implementation-dependant,
-    // and in IE returns signed numbers when used on full words
-    var s="", v;
-    for (var i=7; i>=0; i--) { v = (n>>>(i*4)) & 0xf; s += v.toString(16); }
-    return s;
-};
-
-
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-
-
-/** Extend String object with method to encode multi-byte string to utf8
- *  - monsur.hossa.in/2012/07/20/utf-8-in-javascript.html */
-if (typeof String.prototype.utf8Encode == 'undefined') {
-    String.prototype.utf8Encode = function() {
-        return unescape( encodeURIComponent( this ) );
-    };
-}
-
-/** Extend String object with method to decode utf8 string to multi-byte */
-if (typeof String.prototype.utf8Decode == 'undefined') {
-    String.prototype.utf8Decode = function() {
-        try {
-            return decodeURIComponent( escape( this ) );
-        } catch (e) {
-            return this; // invalid UTF-8? return as-is
+    word_array.push(i);
+    while ((word_array.length % 16) != 14) word_array.push(0);
+    word_array.push(msg_len >>> 29);
+    word_array.push((msg_len << 3) & 0x0ffffffff);
+    for (blockstart = 0; blockstart < word_array.length; blockstart += 16) {
+        for (i = 0; i < 16; i++) W[i] = word_array[blockstart + i];
+        for (i = 16; i <= 79; i++) W[i] = rotate_left(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
+        A = H0;
+        B = H1;
+        C = H2;
+        D = H3;
+        E = H4;
+        for (i = 0; i <= 19; i++) {
+            temp = (rotate_left(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = rotate_left(B, 30);
+            B = A;
+            A = temp;
         }
-    };
+        for (i = 20; i <= 39; i++) {
+            temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = rotate_left(B, 30);
+            B = A;
+            A = temp;
+        }
+        for (i = 40; i <= 59; i++) {
+            temp = (rotate_left(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = rotate_left(B, 30);
+            B = A;
+            A = temp;
+        }
+        for (i = 60; i <= 79; i++) {
+            temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
+            E = D;
+            D = C;
+            C = rotate_left(B, 30);
+            B = A;
+            A = temp;
+        }
+        H0 = (H0 + A) & 0x0ffffffff;
+        H1 = (H1 + B) & 0x0ffffffff;
+        H2 = (H2 + C) & 0x0ffffffff;
+        H3 = (H3 + D) & 0x0ffffffff;
+        H4 = (H4 + E) & 0x0ffffffff;
+    }
+    temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
+    return temp.toLowerCase();
 }
 
 
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-if (typeof module != 'undefined' && module.exports) module.exports = Sha1; // CommonJs export
-if (typeof define == 'function' && define.amd) define([], function() { return Sha1; }); // AMD
+function rotate_left(n, s) {
+    var t4 = ( n << s ) | (n >>> (32 - s));
+    return t4;
+}
+
+function lsb_hex(val) {
+    var str = "";
+    var i;
+    var vh;
+    var vl;
+    for (i = 0; i <= 6; i += 2) {
+        vh = (val >>> (i * 4 + 4)) & 0x0f;
+        vl = (val >>> (i * 4)) & 0x0f;
+        str += vh.toString(16) + vl.toString(16);
+    }
+    return str;
+}
+
+
+function cvt_hex(val) {
+    var str = "";
+    var i;
+    var v;
+    for (i = 7; i >= 0; i--) {
+        v = (val >>> (i * 4)) & 0x0f;
+        str += v.toString(16);
+    }
+    return str;
+}
+
+function Utf8Encode(string) {
+    string = string.replace(/\r\n/g, "\n");
+    var utftext = "";
+    for (var n = 0; n < string.length; n++) {
+        var c = string.charCodeAt(n);
+        if (c < 128) {
+            utftext += String.fromCharCode(c);
+        }
+        else if ((c > 127) && (c < 2048)) {
+            utftext += String.fromCharCode((c >> 6) | 192);
+            utftext += String.fromCharCode((c & 63) | 128);
+        }
+        else {
+            utftext += String.fromCharCode((c >> 12) | 224);
+            utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+            utftext += String.fromCharCode((c & 63) | 128);
+        }
+    }
+    return utftext;
+}
